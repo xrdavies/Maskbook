b14ca1eb30b66b459297c5799ac961a28474da1a
lelenei <zwpsky@gmail.com>
2021-01-29 17:25:15 +0800

    fix: remove debug code

--- packages/maskbook/src/extension/options-page/DashboardRouters/Wallets.tsx
+++ packages/maskbook/src/extension/options-page/DashboardRouters/Wallets.tsx
@@ -25,18 +25,7 @@ import { WalletContent } from '../DashboardComponents/WalletContent'
 import { EthereumStatusBar } from '../../../web3/UI/EthereumStatusBar'
 import { extendsTheme } from '../../../utils/theme'
 import { useAssetsStableCoinDetailedDebank } from '../../../web3/hooks/useAssetsStableCoinDetailedDebank'
-import { getPrice } from '../../../plugins/Trader/apis/coinmarketcap'
-import { subscribeCoinPrce } from '../../../plugins/Trader/apis/coinmarketcap/getPriceStat'
 
-subscribeCoinPrce(1)
-subscribeCoinPrce(825)
-
-try {
-    const price = getPrice(1)
-    console.log(price)
-} catch (e) {
-    console.log(e)
-}
 //#region theme
 const walletsTheme = extendsTheme((theme) => ({
     components: {
